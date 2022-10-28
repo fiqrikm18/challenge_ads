d20fcef64def122727e7dc00a6ed281834a55616
fiqri khoirul m <fiqrikm18@gmail.com>
2022-10-28 09:14:09 +0700

    fix(create-ads): handling error

--- src/controllers/ads.controller.ts
+++ src/controllers/ads.controller.ts
@@ -1,10 +1,13 @@
+import { AxiosError } from "axios";
 import { Request, Response } from "express";
-import { createCampaign } from "../services/ads.service";
+import { ApiError, createCampaign } from "../services/ads.service";
 
 export default class AdsController {
   async createAds(req: Request, res: Response) {
+    const { campaign_name, campaign_objective, campaign_type, campaign_options } = req.body;
+
     try {
-      const data = await (await createCampaign("Test Campaign", "PAGE_LIKES", "PAUSED", [])).data;
+      const data = await (await createCampaign(campaign_name, campaign_objective, campaign_type, campaign_options)).data;
       return res.json({
         "code": 201,
         "msg": "Campaign created",
@@ -13,9 +16,10 @@ export default class AdsController {
         }
       });
     } catch (_e) {
+      const error = _e as AxiosError;
       return res.json({
-        "code": 500,
-        "msg": (_e as Error).message,
+        "code": error.response?.status,
+        "msg": (error.response?.data as ApiError).error.error_user_msg ? (error.response?.data as ApiError).error.error_user_msg : error.message,
         "data": {}
       });
     }
--- src/services/ads.service.ts
+++ src/services/ads.service.ts
@@ -1,12 +1,27 @@
 import axios from "axios";
 import dotenv from "dotenv";
 
-
 dotenv.config();
 
 const baseUrl = process.env.FACEBOOKADS_GRAPH_BASE_URL;
 const accessToken = process.env.FACEBOOKADS_ACCESS_TOKEN;
 const adAccountId = process.env.FACEBOOKADS_AD_ACCOUNT_ID;
+
+export interface ApiError {
+  error: AdsError
+}
+
+interface AdsError {
+  message?: string;
+  type?: string;
+  code?: number;
+  error_data?: object;
+  error_subcode?: number;
+  is_transient?: boolean;
+  error_user_title?: string;
+  error_user_msg?: string;
+  fbtrace_id?: string;
+}
 
 export const createCampaign = async (name: string, objective: string, status: string, adCategory: []) => {
   const response = await axios.post(`${baseUrl}/${adAccountId}/campaigns?access_token=${accessToken}`, {
